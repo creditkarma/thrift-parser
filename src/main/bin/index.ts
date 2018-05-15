8ea44ea86b35ed41901250c7d3924bbf8237af9a
Kevin Greene <kevin.greene@creditkarma.com>
2018-05-15 11:34:32 -0700

    chore: Fix linting

--- src/main/bin/index.ts
+++ src/main/bin/index.ts
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-import * as path from 'path';
-import * as fs from 'fs';
+import * as fs from 'fs'
+import * as path from 'path'
 
-import { mkdir } from './mkdir';
-import { parseFiles, ParseOptions, ThriftDocument } from '../index';
-import { resolveOptions } from './resolveOptions';
+import { parseFiles, ParseOptions, ThriftDocument } from '../index'
+import { mkdir } from './mkdir'
+import { resolveOptions } from './resolveOptions'
 
-const cliArgs: Array<string> = process.argv.slice(2);
-const options: ParseOptions = resolveOptions(cliArgs);
+const cliArgs: Array<string> = process.argv.slice(2)
+const options: ParseOptions = resolveOptions(cliArgs)
 
 parseFiles(options).forEach((ast: ThriftDocument, index: number): void => {
-  const json: string = JSON.stringify(ast, null, 2);
-  const file: string = options.files[index];
-  const outDir: string = path.resolve(process.cwd(), options.rootDir, options.outDir);
-  const outFile: string = path.resolve(outDir, file.replace('.thrift', '.json'));
+  const json: string = JSON.stringify(ast, null, 2)
+  const file: string = options.files[index]
+  const outDir: string = path.resolve(process.cwd(), options.rootDir, options.outDir)
+  const outFile: string = path.resolve(outDir, file.replace('.thrift', '.json'))
 
   // Verify output directory exists, create if it doesn't
-  mkdir(path.dirname(outFile));
+  mkdir(path.dirname(outFile))
 
   // Write json to file
-  fs.writeFileSync(outFile, json, 'utf-8');
-});
+  fs.writeFileSync(outFile, json, 'utf-8')
+})
--- src/main/bin/resolveOptions.ts
+++ src/main/bin/resolveOptions.ts
@@ -7,49 +7,49 @@ import { ParseOptions } from '../index'
  * --fastFail
  */
 export function resolveOptions(args: Array<string>): ParseOptions {
-  const len: number = args.length;
-  let index: number = 0;
+  const len: number = args.length
+  let index: number = 0
   const options: ParseOptions = {
     fastFail: false,
     rootDir: '.',
     outDir: 'thrift-json',
-    files: []
-  };
+    files: [],
+  }
 
   while (index < len) {
-    const next: string = args[index];
+    const next: string = args[index]
 
     switch (next) {
       case '--rootDir':
-        options.rootDir = args[(index + 1)];
+        options.rootDir = args[(index + 1)]
         try {
           if (lstatSync(options.rootDir).isDirectory()) {
-            index += 2;
-            break;
+            index += 2
+            break
           } else {
-            throw new Error(`Provided root directory "${options.rootDir}" isn't a directory`);
+            throw new Error(`Provided root directory "${options.rootDir}" isn't a directory`)
           }
-        } catch(e) {
-          throw new Error(`Provided root directory "${options.rootDir}" doesn't exist`);
+        } catch (e) {
+          throw new Error(`Provided root directory "${options.rootDir}" doesn't exist`)
         }
 
       case '--outDir':
-        options.outDir = args[(index + 1)];
-        index += 2;
-        break;
+        options.outDir = args[(index + 1)]
+        index += 2
+        break
 
       case '--fastFail':
-        options.fastFail = args[(index + 1)] === 'true';
-        index += 2;
-        break;
+        options.fastFail = args[(index + 1)] === 'true'
+        index += 2
+        break
 
       default:
         if (next.startsWith('--')) {
-          throw new Error(`Unknown option provided to generator "${next}"`);
+          throw new Error(`Unknown option provided to generator "${next}"`)
         } else {
           // Assume option is a file to parse
-          options.files.push(next);
-          index += 1;
+          options.files.push(next)
+          index += 1
         }
     }
   }
